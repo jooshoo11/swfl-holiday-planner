f58bba95b3584468c40319082bfda88652433dfd
jooshoo11 <jhukill0118@gmail.com>
2026-09-17 13:55:03 -0400

    Add dynamic seasonal ambient particle effects (leaves, snowflakes, coastal bokeh lights) with on/off toggle

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for SWFL Holiday & Festivities Planner
-const CACHE_NAME = 'swfl-holidays-v1';
+const CACHE_NAME = 'swfl-holidays-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
